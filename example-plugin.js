a06f9db68f24ddb974a03d00f3e18706c4901ad9
Matt Zeunert <matt@mostlystatic.com>
2016-07-18 13:18:25 +0100

    Don't use ES6 in example plugin code.

--- example-plugin.js
+++ example-plugin.js
@@ -1,7 +1,7 @@
 module.exports = function(babel) {
     return {
         visitor: {
-            Literal(path) {
+            Literal: function(path) {
                 // not in the slides, but if we always replace all strings that kinda
                 // breaks the other demo a bit
                 if (path.node.value !== "Hello") {
