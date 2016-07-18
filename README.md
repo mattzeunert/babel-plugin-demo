cec0aeed305c64ed632655850f05262b73b513b0
Matt Zeunert <matt@mostlystatic.com>
2016-07-18 13:47:07 +0100

    Complete.

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Demo code for my [lightning talk on Babel](http://slides.com/mattzeunert/javascr
 To get set up run `npm install`.
 
 Use `npm run babel-example` to compile example-demo.js.
+
+Use `npm run import-demo` to compile subfolder/app.js.
--- root-import-plugin.js
+++ root-import-plugin.js
@@ -1,7 +1,18 @@
 module.exports = function(babel) {
     return {
         visitor: {
+            ImportDeclaration: function(path){
+                var filepath = path.node.source.value;
 
+                if (filepath[0] !== "@") {
+                    return;
+                }
+
+                filepath = filepath.substring(1)
+                filepath = process.cwd() + "/import-demo/" + filepath;
+
+                path.node.source.value = filepath;
+            }
         }
     }
 }
